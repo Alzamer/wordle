ecc004c7b8243817effe096f289318551cd2066c
alzamer <mic.szendzielorz@gmail.com>
2023-08-07 23:14:12 +0200

    Added files

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+import Header from './Components/Header/';
+import Game from './Components/Game/';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Game/>
+    </div>
+  );
+}
+
+export default App;
