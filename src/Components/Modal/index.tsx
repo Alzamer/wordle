8baf68051e4fd9220218f0c68f9b465dec6b9ca7
alzamer <mic.szendzielorz@gmail.com>
2023-10-13 21:41:26 +0200

    Added RWD

--- src/Components/Modal/index.tsx
+++ src/Components/Modal/index.tsx
@@ -1,18 +1,18 @@
 import styles from './Modal.module.css'
+import { useState } from 'react'
 
 export default function Modal(){
-	const handleClick = (target: any) => {
-        if(target.target.className.startsWith("Modal_info")){
+    const [showModal, setShowModal] = useState(true)
 
-        }
-        else{
-
+    const handleClick = (target: any) => {
+        if(!target.target.className.startsWith("Modal_info")){
+            setShowModal(!showModal)
         }
     }
     
-    return <div className={styles.container} onClick={handleClick}>
+    return <div className={`${styles.container} ${ showModal ? null : styles.dontShowMe}`} onClick={handleClick}>
         <div className={styles.info}>
-            West Coast Shit!
+            
         </div>
 	</div>
 }
